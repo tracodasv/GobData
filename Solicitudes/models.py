801a5c71a0fc2194183e4534b23428ec69cd790d
Roberto Marroquin <robma96@gmail.com>
2018-11-11 14:33:23 -0600

    Creacion de modelos de Solicitudes

--- Solicitudes/models.py
+++ Solicitudes/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
-
+from django.urls import reverse
 
 class Solicitud(models.Model):
 
-    
+    fechaCreacion = models.DateField(("Apertura De Solicitud"),auto_now=False, auto_now_add=True)
+    fechaModificacion = models.DateField(("Ultima Modificacion"),auto_now=True, auto_now_add=False)
+    estado = models.CharField(("Estado"), max_length=100)
+    etapa = models.ForeignKey("Etapa", on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = _("Solicitud")
-        verbose_name_plural = _("Solicitudes")
+        verbose_name = ("Solicitud")
+        verbose_name_plural = ("Solicitudes")
 
     def __str__(self):
         return self.name
@@ -16,3 +18,51 @@ class Solicitud(models.Model):
     def get_absolute_url(self):
         return reverse("Solicitud_detail", kwargs={"pk": self.pk})
 
+
+class DetalleSolicitud(models.Model):
+    
+    firmaSolicitante = models.ImageField(("Firma"), upload_to=None, height_field=None, width_field=None, max_length=None)
+    solicitante = models.CharField(("Solicitante"), max_length=200)
+    solicitud = models.ForeignKey("Solicitud", on_delete=models.CASCADE) 
+
+    class Meta:
+        verbose_name = ("DetalleSolicitud")
+        verbose_name_plural = ("DetalleSolicitudes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("DetalleSolicitud_detail", kwargs={"pk": self.pk})
+
+
+class Requerimiento(models.Model):
+    categoria = models.CharField(("Categoria"),blank=True, max_length=50)
+    detalle = models.TextField(("Lo que solicita"))
+    detalleSolicitud = models.ForeignKey("DetalleSolicitud", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Requerimiento")
+        verbose_name_plural = ("Requerimientos")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Requerimiento_detail", kwargs={"pk": self.pk})
+
+
+class Etapa(models.Model):
+    progreso = models.IntegerField(("Progreso de Solicitud"))
+    nombreEtapa = models.CharField(("Etapa"), max_length=50)
+    
+
+    class Meta:
+        verbose_name = ("Etapa")
+        verbose_name_plural = ("Etapas")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Etapa_detail", kwargs={"pk": self.pk})
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy 
 
-# Create your views here.
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = "usuarios/registrar.html"
+    form_class = UserCreationForm
+    succes_url= reverse_lazy('')
+    
